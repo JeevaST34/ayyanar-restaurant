68ef394a9137fae81bf0d57955d0c8bdf0720a32
JeevaST34 <jeevanandam@scopethinkers.com>
2026-05-20 11:59:56 +0530

    docker & config files updated

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+
   turbopack: {
     root: process.cwd(),
   },
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
